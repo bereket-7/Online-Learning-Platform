7ddb49b2f631bddcaabfc47e09623b314398fa73
bereket-7 <bekhonelign@gmail.com>
2024-04-24 22:59:46 +0300

    updated setting

--- olp/urls.py
+++ olp/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, HomeView, ProfileView, SignUpView
+"""
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 
+from django.urls import path
+from .views import CustomLoginView, HomePageView, RegisterPage
 
 urlpatterns = [
-    path('', HomeView, name='home'),
-    path('accounts/login/', CustomLoginView.as_view(), name='login'),
-    path('accounts/logout/', CustomLogoutView.as_view(), name='logout'),
-    path('accounts/signup/', SignUpView.as_view(), name='signup'),
-    path('accounts/profile/', ProfileView, name='profile'),
+     path('', HomePageView.as_view(), name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('register/', RegisterPage.as_view(), name='register'),
 ]
